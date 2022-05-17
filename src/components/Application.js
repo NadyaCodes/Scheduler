dc6c96191b157286bd9bdf8ef70aeaf3f789adda
NadyaCodes <nadyacorscadden@gmail.com>
2022-05-17 16:53:31 -0400

    add strict mode to index.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -35,6 +35,7 @@ export default function Application(props) {
   
   return (
     <main className="layout">
+
       <section className="sidebar">
         <img
           className="sidebar--centered"
@@ -43,7 +44,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay}/>
+          <DayList days={days} day={day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
