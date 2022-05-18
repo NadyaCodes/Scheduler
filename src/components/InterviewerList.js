0e53dd757d948aa92f1cf97a1053ea7abd6357f0
NadyaCodes <nadyacorscadden@gmail.com>
2022-05-17 21:23:11 -0400

    refactor interview components to bypass passing id as a prop

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,17 +4,19 @@ import InterviewerListItem from "components/InterviewerListItem.js"
 
 export default function InterviewerList(props) {
 
-  const {interviewers, setInterviewer, interviewer} = props;
+  // const {interviewers, setInterviewer, interviewer} = props;
 
-  const allInterviewers = props.interviewers.map(x => (
-    <InterviewerListItem
-      id={x.id}
-      name={x.name}
-      avatar={x.avatar}
-      setInterviewer={props.setInterviewer}
-      selected={props.interviewer === x.id}
+  const allInterviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
       />
-    ))
+    );
+  });
 
   return(
     <section className="interviewers">
@@ -24,6 +26,14 @@ export default function InterviewerList(props) {
   )
 
 }
+
+// .add("Clickable", () => (
+//   <InterviewerListItem
+//     name={interviewer.name}
+//     avatar={interviewer.avatar}
+//     setInterviewer={() => action("setInterviewer")(interviewer.id)}
+//   />
+// ));
 
 
 // const allDays = props.days.map(x => (
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -13,16 +13,16 @@ export default function InterviewerListItem(props) {
     " interviewers__item--selected": props.selected
   })
 
-  return(
-    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
+  return (
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.selected && <>{props.name}</>}
+      {props.selected && props.name}
     </li>
-  )
+  );
 }
 
 
