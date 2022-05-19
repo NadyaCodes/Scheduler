797773e3ee4f10434165ac550672db4864722c13
NadyaCodes <nadyacorscadden@gmail.com>
2022-05-18 20:11:21 -0400

    add appointment schedule into Application.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from "components/Appointment/Header.js"
 import Show from "components/Appointment/Show.js"
 import Empty from "components/Appointment/Empty.js"
 
+
 export default function Appointment(props) {
   return(
     <article className="appointment">
