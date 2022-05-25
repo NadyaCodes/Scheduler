7045e22a69fade5c5b24251626d0a1f8cab6b5ad
NadyaCodes <nadyacorscadden@gmail.com>
2022-05-25 14:23:30 -0400

    add first cypress tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -13,7 +13,7 @@ export default function DayListItem(props) {
 
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass} selected={props.selected}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass} selected={props.selected} data-testid="day">
       <h2 className="text--regular" >{props.name}</h2>
       {(props.spots > 1) && <h3 className="text--light" >{props.spots} spots remaining</h3>}
       {(props.spots === 1) && <h3 className="text--light" >{props.spots} spot remaining</h3>}
